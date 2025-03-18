508d60fcd7587a89f6d5d17faa39e184397c9678
AathithanN <Aathithan.navaneetharaja@gmail.com>
2025-03-18 18:47:50 +0000

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,10 @@
+# 23 x 1 = 23
+
+for n in range(1,11):
+    print(23," x ",n," = ",23*n)
+    
+# Taking a input from the user using the input function
+
+m=int(input("Type a number"))
+for n in range(1,11):
+    print(m," x ",n," = ",m*n)
